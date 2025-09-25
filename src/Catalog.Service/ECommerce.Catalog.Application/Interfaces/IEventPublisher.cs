126fc9204c2c91dfece0703696e766f98c6f6fa8
Allyson Reis <allysonreis1@gmail.com>
2025-09-25 18:20:29 -0400

    feat: add IEventPublisher interface for domain event publishing

---
+++ src/Catalog.Service/ECommerce.Catalog.Application/Interfaces/IEventPublisher.cs
@@ -0,0 +1,7 @@
+﻿using ECommerce.Catalog.Domain.Events.shared;
+
+namespace ECommerce.Catalog.Application.Interfaces;
+public interface IEventPublisher
+{
+    Task PublishAsync(IDomainEvent domainEvent, string topic, CancellationToken cancellationToken);
+}
